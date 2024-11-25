de3241cd6d1f76a8bb84735a636ef1cc7319f10c
Nikitososs <nsmirnow.n@gmail.com>
2024-11-25 12:16:08 +0600

    Update 2.cs

--- Алгоритмизция и программиование/1 семестр/22.11/2.cs
+++ Алгоритмизция и программиование/1 семестр/22.11/2.cs
@@ -4,7 +4,7 @@ string inp = Convert.ToString(Console.ReadLine());
 while (inp.Length>0)
 {
     string inp2 = inp.ToLower();
-    inp2 = inp2.Replace("abc", "***").Replace("ab", "**").Replace("a", "*");
+    inp2 = inp2.Replace("abc", "3").Replace("ab", "2").Replace("a", "1");
     char[] inparr = inp2.ToCharArray();
     Console.WriteLine(dl(inparr));
     
@@ -14,17 +14,20 @@ while (inp.Length>0)
 
 static int dl(char[] arr)
 {
-    int cnt = 1;
+    int cnt = 0;
     int mx = 0;
     
     for (int i=0;i < arr.Length-1; i++)
     {      
-        if (arr[i] == arr[i+1] && arr[i] == '*')
-        {cnt += 1;}
+        if ((arr[i] == arr[i+1] && arr[i] == '3')||(arr[i] == '3' && arr[i+1] == '2')||(arr[i] == '3' && arr[i+1] == '1'))
+        {
+            cnt = cnt + int.Parse(arr[i].ToString()) + int.Parse(arr[i+1].ToString());
+            if (i > 0 && arr[i-1] == '3')
+            {cnt -= int.Parse(arr[i].ToString());}
+        }
         else
-        {mx = Math.Max(mx, cnt); cnt = 1;}
+        {mx = Math.Max(mx, cnt); cnt = 0;}
     }
     mx = Math.Max(mx, cnt);
     return mx;
 }
-
