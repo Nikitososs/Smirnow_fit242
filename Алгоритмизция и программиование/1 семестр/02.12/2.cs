5c2d852e4c675a2c877fdd0d29b4ad67507ccc9d
Nikitososs <nsmirnow.n@gmail.com>
2024-12-24 09:38:10 +0600

    Update 2.cs

--- Алгоритмизция и программиование/1 семестр/02.12/2.cs
+++ Алгоритмизция и программиование/1 семестр/02.12/2.cs
@@ -1,26 +1,39 @@
-while (true)
+using System;
+namespace Wtf
 {
-    Console.WriteLine("Введите строку:");
-    Xz a = new Xz() {data = Console.ReadLine()};
-    Console.WriteLine(a.Chet());
-}
-class Xz 
-{
-    private string _data;
-    public string data {get {return _data;} set {_data = value;}}
-
-    public Xz(){}
-
-    public bool Chet()
+    class Programm
     {
-        char[] data_char = _data.ToCharArray();
-        for (int i = 1; i < data_char.Length-1; i++)
+        public static void Main()
         {
-            if (!char.IsDigit(data_char[i]))
+            while (true)
             {
-                if (Convert.ToInt32(data_char[i-1])%2==0){return true;}
+                Console.WriteLine("Введите строку:");
+                Xz a = new Xz() {data = Console.ReadLine()};
+                Console.WriteLine(a.Chet());
             }
         }
-        return false;
+    }
+    class Xz 
+    {
+        private string _data;
+        public string data {get {return _data;} set {_data = value;}}
+
+        public Xz(){}
+
+        public bool Chet()
+        {
+            char[] data_char = _data.ToCharArray();
+            for (int i = 0; i < data_char.Length-1; i++)
+            {
+                if (!char.IsDigit(data_char[i+1]))
+                {
+                    if (char.IsDigit(data_char[i]))
+                    {if (Convert.ToInt32(data_char[i])%2==0){return true;}}
+                }
+            }
+            if (char.IsDigit(data_char[data_char.Length-1])){return true;}
+
+            return false;
+        }
     }
 }
