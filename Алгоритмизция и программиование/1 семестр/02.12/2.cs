344603b7955d7fd1ef722b690147da0c9b1d93e7
Nikitososs <nsmirnow.n@gmail.com>
2025-01-12 12:03:14 +0600

    Update 2.cs

--- Алгоритмизция и программиование/1 семестр/02.12/2.cs
+++ Алгоритмизция и программиование/1 семестр/02.12/2.cs
@@ -23,16 +23,16 @@ namespace Wtf
         public bool Chet()
         {
             char[] data_char = _data.ToCharArray();
+            if (data_char.Length == 1){if (char.IsDigit(data_char[0])){if (Convert.ToInt32(data_char[0])%2==0){return true;}}}
             for (int i = 0; i < data_char.Length-1; i++)
             {
-                if (!char.IsDigit(data_char[i+1]))
-                {
-                    if (char.IsDigit(data_char[i]))
-                    {if (Convert.ToInt32(data_char[i])%2==0){return true;}}
-                }
+                if (i == data_char.Length-2 && char.IsDigit(data_char[i+1]))
+                {if (Convert.ToInt32(data_char[i+1])%2==0){return true;}}
+                else if (char.IsDigit(data_char[i]) && !char.IsDigit(data_char[i+1]))
+                {if (Convert.ToInt32(data_char[i])%2==0){return true;}}
+                else if (char.IsDigit(data_char[i]) && char.IsDigit(data_char[i+1]))
+                {continue;}
             }
-            if (char.IsDigit(data_char[data_char.Length-1])){return true;}
-
             return false;
         }
     }
