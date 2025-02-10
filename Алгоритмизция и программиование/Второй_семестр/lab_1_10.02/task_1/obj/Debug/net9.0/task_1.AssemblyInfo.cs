8d38c07363556b2db6aac9a5708df1930838155b
Nikitososs <nikitosossss@yandex.ru>
2025-02-10 19:08:18 +0600

    a

--- Алгоритмизция и программиование/Второй_семестр/lab_1_10.02/task_1/obj/Debug/net9.0/task_1.AssemblyInfo.cs
+++ Алгоритмизция и программиование/Второй_семестр/lab_1_10.02/task_1/obj/Debug/net9.0/task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08c0752d1b81b5ba7d5744c4275c581906d2d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49a5eb24cbaf851d0567e7f5da6de276b824216")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
